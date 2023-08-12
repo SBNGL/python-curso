ba6be875f64975de41cb96ab1d6c48e65006556b
= <=>
2023-08-12 00:17:51 -0500

    version 2

--- practica1.py
+++ practica1.py
@@ -1,21 +1,24 @@
+def descuentos(consumo):
+    if consumo > 0 and consumo <= 100:
+        descuento = consumo * 0.10
+    elif consumo > 100 and consumo <= 200:
+        descuento = consumo * 0.20    
+    elif consumo > 200:
+        descuento = consumo * 0.30
+   
+    return descuento
+
 consumo = float(input('Ingrese el consumo del cliente '))
 
-
-if consumo > 0 and consumo <= 100:
-    descuento = consumo * 0.10
-elif consumo > 100 and consumo <= 200:
-    descuento = consumo * 0.20    
-elif consumo > 200:
-     descuento = consumo * 0.30
-else:
-    descuento = 0
-    print('Consumo invalido')
+def impuesto(monto):
+    return monto * 0.15
     
 if(consumo > 0):
-    total = consumo - descuento
-    totalpago = total + total * 0.15 
+    total = consumo - descuentos(consumo)
+    totalpago = total + impuesto(total)
 
-print( 'Descuento: ',descuento)
-print('Total: ', total)
-print('Total + impuesto: ', totalpago)       
-    
+    print( 'Descuento: ',descuentos(consumo))
+    print('Total: ', total)
+    print('Total + impuesto: ', totalpago)       
+else:
+    print('Dato incorrecto')    
--- practica2.py
+++ practica2.py
@@ -4,15 +4,18 @@ num = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 pares = []
 impares = []
 
+def agregarArreglos(num, n, aleatorio):
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+        
+    print(f'{n} x {aleatorio} = {num}')    
+
+
 for n in num:
     aleatorio = random.randint(1, 100)
-    result = n * aleatorio
-    
-    print(f'{n} x {aleatorio} = {result}')
-    if result % 2 == 0:
-        pares.append(result)
-    else:
-        impares.append(result)
+    agregarArreglos(n * aleatorio, n, aleatorio)
 
 print('Lista de Pares: ', pares )
 print('Lista de impares: ', impares)
