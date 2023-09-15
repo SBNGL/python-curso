7ffde040de18e0f3904c8fe5cebccceb4c720f1b
= <=>
2023-09-14 22:11:31 -0500

    primera version

---
+++ Logica.py
@@ -0,0 +1,29 @@
+
+soy_yo = True
+no_soy_yo = False
+
+"""""
+if soy_yo and no_soy_yo:
+    print('soy yo y no soy yo')
+elif soy_yo:
+    print('Soy yo')
+elif no_soy_yo:
+    print('No soy yo')   
+"""""
+#Operador ternario
+# condicion_verdadera if condicion else condicion falsa
+#mensaje = 'Soy yo y no soy yo' if soy_yo or no_soy_yo else 'No soy yo'
+#print(mensaje)
+
+#is vs ==
+print(True == 1)
+print("" == 1)
+print([] == 1)
+print(10 == 10.0)
+print([] == [])
+print()
+print(True is 1)
+print("" is 1)
+print([] is 1)
+print(10 is 10.0)
+print([] is [])     
