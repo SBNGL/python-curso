dd2e85c70705c7464e00dd92febc164dc882431b
= <=>
2023-09-14 22:36:34 -0500

    version 1

---
+++ for_while.py
@@ -0,0 +1,18 @@
+""""
+for s, char in enumerate('sebastian'):
+    print(s, char)
+    
+for j, num in enumerate(list(range(50))):
+    print(j, num)
+    if (num == 18):
+        print(f'index de 18: {j}')    
+"""
+#For and While
+
+for i in ([1, 2, 3]):
+    print(i)
+print()   
+i = 1
+while i<=3:
+    print(i)
+    i+=1           
